c66ce1bba0a227b5c3df9cfc291539a6edb5bad2
YahyaAdnan <mr.yahyaadnan@gmail.com>
2024-01-19 13:53:30 +0300

    Table Crud

---
+++ app/Filament/Resources/TableResource.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\TableResource\Pages;
+use App\Filament\Resources\TableResource\RelationManagers;
+use App\Models\Table as Seating;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Forms\Components\TextInput;
+use Filament\Tables\Columns\TextColumn;
+use Filament\Tables\Filters\SelectFilter;
+use Filament\Forms\Components\Select;
+use Filament\Forms\Components\Textarea;
+use Filament\Resources\Resource;
+use Filament\Tables\Enums\FiltersLayout;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+use Illuminate\Support\Facades\Auth;
+
+class TableResource extends Resource
+{
+    protected static ?string $model = Seating::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-cube';
+
+    public static function canCreate(): bool
+    {
+        return Auth::user()->authorized('create tables');
+    }
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                TextInput::make('title')->columnSpan(6)->required()->minLength(1),
+                TextInput::make('chairs')->numeric()->columnSpan(6)->required()->minValue(0),
+                Textarea::make('note')->columnSpan(12)
+            ])->columns(12);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                TextColumn::make('title')->sortable()->searchable(),
+                TextColumn::make('chairs')->sortable(),
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListTables::route('/'),
+            'create' => Pages\CreateTable::route('/create'),
+            'edit' => Pages\EditTable::route('/{record}/edit'),
+        ];
+    }
+}
