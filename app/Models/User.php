730c6c093557ef1e4ed230b1a3baddd1becc7aeb
YahyaAdnan <mr.yahyaadnan@gmail.com>
2024-05-04 08:34:02 +0300

    User filament

--- app/Models/User.php
+++ app/Models/User.php
@@ -48,6 +48,10 @@ class User extends Authenticatable
         'password' => 'hashed',
     ];
       
+    public function canAccessPanel(Panel $panel): bool
+    {
+        return true;
+    }
 
     public function role()
     {
