1fdd38f466d1ded8a9011593bd276944fc420fcc
00bx <zamanmerani49@gmail.com>
2024-05-19 13:24:07 +0300

    table and form

--- resources/views/livewire/invoice/new-orders.blade.php
+++ resources/views/livewire/invoice/new-orders.blade.php
@@ -1,8 +1,8 @@
-<div class="grid grid-cols-4 mt-4">
-    <div class="col-span-2 m-1">
+<div class="grid grid-cols-4 mt-4 ">
+    <div class="col-span-2 m-1 md:w-1/3 sm:w-full  ">
         {{$this->table}}
     </div>
-    <div class="col-span-2 m-1">
+    <div class="col-span-2 m-1 md:w-1/4 sm:w-full ">
         <form wire:submit="create">
             {{-- TO-DO: make it card with submit and cancel --}}
             <div class="flex justify-center mt-5">
@@ -10,7 +10,7 @@
                     Submit
                 </button>
             </div>
-            {{ $this->form }} 
+            {{ $this->form }}
         </form>
     </div>
 </div>
