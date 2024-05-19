924fb561651050a348f25d55f5d9cc6865b4b5b8
00bx <zamanmerani49@gmail.com>
2024-05-20 00:05:16 +0300

    form and table

--- resources/views/livewire/invoice/new-orders.blade.php
+++ resources/views/livewire/invoice/new-orders.blade.php
@@ -1,25 +1,36 @@
-<div class="grid grid-cols-1 md:grid-cols-4 gap-4 mt-10" style="margin-top: 10px">
+<div class="flex flex-col lg:flex-row gap-4 mt-10">
     <!-- Table Section -->
-    <div class="md:col-span-3 w-full">
+    <div class="w-full lg:w-3/8 mt-3">
         {{$this->table}}
     </div>
 
     <!-- Form Section -->
-    <div class="md:col-span-1 w-full">
+    <div class="w-full lg:w-1/9 card bg-white mt-3 rounded">
+
         <form wire:submit.prevent="create">
-            <div class="card bg-white p-4 shadow-md rounded-md">
+            <div class="p-4 shadow-md rounded-md">
                 <div class="card-body">
                     {{ $this->form }}
                 </div>
                 <div class="card-footer flex justify-between">
-                    <button style="background-color: #635ffd" type="submit" class="btn btn-primary py-2 px-4 rounded text-white hover:bg-blue-600">
+                    <button style="background-color: #635ffd" type="submit" class="btn btn-primary py-2 px-4 rounded text-white hover:bg-blue-600 ">
                         Submit
-                    </button>
-                    <button type="button" class="btn btn-secondary py-2 px-4 rounded bg-gray-500 text-white hover:bg-gray-600" wire:click="cancel">
-                        Cancel
                     </button>
                 </div>
             </div>
         </form>
     </div>
 </div>
+@assets
+<style>
+
+    @media (min-width: 1524px) {
+        .lg\:w-3\/8 {
+            width: 95%;
+        }
+        .lg\:w-1\/9 {
+            width: 40%;
+        }
+    }
+</style>
+@endassets
