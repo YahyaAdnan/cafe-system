24b14347771e610f202102af315eaf201dc4f061
00bx <zamanmerani49@gmail.com>
2024-11-29 15:37:33 +0300

    new ui

--- resources/views/livewire/invoice/new-orders.blade.php
+++ resources/views/livewire/invoice/new-orders.blade.php
@@ -3,7 +3,7 @@
     <span class="inline-block text-sm font-semibold px-3 py-1 rounded-full shadow mt-1 mb-1" style="background-color: {{App\Models\Setting::get("color") ?? "#3b82f6"}};">
 <div class="flex items-center gap-2">
  <span>{{$order['title']}} x {{$order['quantity']}}</span>
-        <x-filament::icon-button icon="heroicon-o-x-mark" class="w-4 h-4 text-gray-500" wire:click="reduceQuantity({{$order['item_id']}})" />
+        <x-filament::icon-button style="border-width:0px !important" icon="heroicon-o-x-mark" class="w-4 h-4 text-gray-500" wire:click="reduceQuantity({{$order['item_id']}})" />
 </div>
     </span>
     @endforeach
